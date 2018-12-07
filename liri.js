1fdab908e0b284e4f37f16d57a30a8ae4acb504b
jtibbetts1998 <justin.tibbetts98@gmail.com>
2018-12-07 14:41:24 -0500

    adding initial functions and finishing commands

--- liri.js
+++ liri.js
@@ -1,1 +1,35 @@
-require("dotenv").config();
+//Initializing and requiring each command
+let dotenv = require("dotenv").config();
+let keys = require("./keys.js");
+let fs = require("fs");
+let moment = require("moment");
+let Spotify = require('node-spotify-api');
+let spotify = new Spotify('keys.spotify');
+let axios = require("axios");
+let command = process.argv[2];
+let userChoice = process.argv.splice(3).join(" ");
+
+if (userChoice) {
+    userChoice = ;
+}
+
+let userCommand = ;
+
+fs.appendFile("log.txt", userCommand, function(err) {
+    if (err) {
+        console.log(err);
+    }
+})
+
+function getConcertInfo() {
+    axios.get("" + userChoice + "")
+    axios.then(function(response) {
+        let concertTitle = "\n" + userChoice + "next show\n"
+        console.log(concertTitle);
+        fs.appendFile("log.txt", concertTitle, function(err) {
+            if (err) {
+                console.log(err);
+            }
+        }
+    }
+})
